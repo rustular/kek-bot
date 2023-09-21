88cb924dc37a0813194650846bddf2f52a3dafc3
John Bakhmat <johnbakhmat@gmail.com>
2023-09-21 15:32:29 +0300

    Now saving who used what kek

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::sync::Arc;
+
+use prisma::PrismaClient;
 use regex::Regex;
 use serenity::async_trait;
 use serenity::model::channel::Message;
@@ -5,8 +8,16 @@ use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
 mod env;
+#[allow(warnings, unused)]
+mod prisma;
 
 struct Handler;
+
+struct DbConnection;
+
+impl TypeMapKey for DbConnection {
+    type Value = Arc<PrismaClient>;
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -17,8 +28,8 @@ impl EventHandler for Handler {
             }
         }
 
-        kek_counter(&msg);
-        kek_fixer(&ctx,&msg).await;
+        kek_counter(&ctx, &msg).await;
+        kek_fixer(&ctx, &msg).await;
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
@@ -26,7 +37,12 @@ impl EventHandler for Handler {
     }
 }
 
-fn kek_counter(msg: &Message) {
+async fn kek_counter(ctx: &Context, msg: &Message) {
+    let db = {
+        let data = ctx.data.read().await;
+        data.get::<DbConnection>().expect("Oops! Can't get db connection").clone()
+    };
+    
     let has_kek = msg.content.to_lowercase().contains("kek");
     if has_kek {
         let kek_regex = Regex::new(r"(?mU).*:(?P<kek>.*[kK][eE][kK].*):.*").unwrap();
@@ -36,11 +52,12 @@ fn kek_counter(msg: &Message) {
         for cap in keks {
             let kek = cap.name("kek").unwrap().as_str();
             println!("{}  {}", user, kek);
+            db.kek_usage().create(kek.to_string(), user.to_string(),vec![]).exec().await;
         }
     }
 }
 
-async fn kek_fixer( ctx: &Context,msg: &Message) {
+async fn kek_fixer(ctx: &Context, msg: &Message) {
     let has_cursed_kek = msg.content.contains(":KEKWt:");
     let kek_suffer =
         "https://cdn.discordapp.com/emojis/1069468390231654561.webp?size=96&quality=lossless";
@@ -60,6 +77,13 @@ async fn main() {
         .event_handler(Handler)
         .await
         .expect("Err creating client");
+
+    let db_client = PrismaClient::_builder().build().await.unwrap();
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<DbConnection>(Arc::new(db_client));
+    }
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
