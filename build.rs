99ebfafe1f671e7fd93a082209d739517cdc549e
John Bakhmat <johnbakhmat@gmail.com>
2023-09-30 14:23:19 +0300

    trying using wallace-minion's build

--- build.rs
+++ build.rs
@@ -2,7 +2,15 @@ fn main() {
     // Run prisma generate if schema changed
     // println!("cargo:rerun-if-changed=prisma/schema.prisma"); // The double deploy hack struggled with this, so left it out
     if !std::process::Command::new("cargo")
-        .args(["run", "prisma", "generate"])
+        .args([
+            "run",
+            "-p",
+            "prisma-cli",
+            "--target-dir",
+            "target_prisma-cli",
+            "--",
+            "generate",
+        ])
         .status()
         .expect("failed to build prisma")
         .success()
